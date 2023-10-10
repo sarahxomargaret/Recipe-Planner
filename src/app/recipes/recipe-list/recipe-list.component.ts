e9cea91b05c1b013f25ab56834e35564cc480ca7
Fawn <sarahmargaret@ymail.com>
2023-10-10 15:28:03 -0400

    added recipe list container

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
-  recipes = [];
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2017/09/16/19/21/salad-2756467_1280.jpg'),
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
